96c1e5c5fdaffde0abb422f6028b401ce5dd917b
toanak <khanhtoanverii@gmail.com>
2024-01-04 23:20:59 +0700

    refactor(helper): styles & scripts

--- main.js
+++ main.js
@@ -11,12 +11,15 @@ const [darkMode, setDarkMode] = useState(document.getElementById('dark'));
  */
 const [url, setUrl] = useState(document.getElementById('inputUrl'));
 const [urlOutput, setUrlOutput] = useState(document.getElementById('decodedUrlOutput'));
+const [urlAction, setUrlAction] = useState(document.getElementById('actionUrl'));
 
 const [string, setString] = useState(document.getElementById('inputString'));
 const [stringOutput, setStringOutput] = useState(document.getElementById('stringLengthOutput'));
+const [stringAction, setStringAction] = useState(document.getElementById('actionString'));
 
 const [json, setJson] = useState(document.getElementById('inputJson'));
 const [jsonOutput, setJsonOutput] = useState(document.getElementById('formattedJsonOutput'));
+const [jsonAction, setJsonAction] = useState(document.getElementById('actionJson'));
 
 /**
  * Function Declaration
@@ -62,4 +65,12 @@ liveMode().addEventListener('change', () => {
         json().removeEventListener('input', formatJSON);
     }
 });
+
+darkMode().addEventListener('change', () => {
+    document.body.classList.toggle('dark-mode');
+})
+
+urlAction().addEventListener('click', decodeURL);
+jsonAction().addEventListener('click', formatJSON);
+stringAction().addEventListener('click', getStringLength);
 
